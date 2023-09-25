6800594de11c0ff0ecb6a1f9e575a4610355eb3f
VugarEyvazov <eyvazov.i.vugar@gmail.com>
2023-09-25 10:49:13 +0400

    auth was added

---
+++ src/stores/createAuth.js
@@ -0,0 +1,44 @@
+import Cookies from "js-cookie";
+
+const handleAuth = async (set, get) => {
+  if (window.location.hostname === "localhost") {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("username", "Vugar.i.eyvazov");
+    urlencoded.append("password", "init123Q");
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    try {
+      const response = await fetch(
+        window.mainUrl + "/api/v1/auth",
+        requestOptions
+      );
+      const json = await response.json();
+      Cookies.set("LLCookie", json.ticket);
+      set({
+        ticket: json.ticket,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    set({
+      ticket: window.OTCSTicket,
+    });
+  }
+};
+
+const createAuth = (set, get) => ({
+  ticket: "",
+  auth: async () => await handleAuth(set, get),
+});
+
+export default createAuth;
